d8c86844191708a8f22b76c65a63718394c0cf59
Andrew P <apettus3@student.alamo.edu>
2022-08-25 13:05:18 -0700

    Create Chap1Lab1.cpp

---
+++ Chap1Lab1.cpp
@@ -0,0 +1,14 @@
+//
+// Chap1Lab1.cpp
+// Written by Andrew Pettus on 8/24/2022
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// is this note needed? :)
+	cout << "What's up, Globe!?! \n";
+	cout << "I, Andrew Pettus, am excited to learn C++! \n";
+	return 0;
+}

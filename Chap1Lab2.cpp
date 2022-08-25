ac560ff28b0e1d218704d1e2008f181fe2a56911
pettuslettuce <apettus3@student.alamo.edu>
2022-08-25 16:23:23 -0500

    Create Chap1Lab2.cpp
    First submit of Chapter 1 Lab 2

---
+++ Chap1Lab2.cpp
@@ -0,0 +1,15 @@
+//
+// Chap1Lab1.cpp
+// Written by Andrew Pettus on 8/24/2022
+// Modified line 11 to include "Hello World" on 8/25/2022
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "***************************** \n";
+	cout << "*     Lab 2 - COSC-1336     * \n";
+    cout << "***************************** \n";
+	return 0;
+}

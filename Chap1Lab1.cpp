05cb21ef42a7927035a94dbfcddae6a78c6b6a23
Andrew P <apettus3@student.alamo.edu>
2022-08-26 13:34:48 -0700

    removed note
    removed note from line 11

--- Chap1Lab1.cpp
+++ Chap1Lab1.cpp
@@ -1,14 +1,13 @@
 //
 // Chap1Lab1.cpp
 // Written by Andrew Pettus on 8/24/2022
-// Modified line 11 to include "Hello World" on 8/25/2022
-//
+// Modified line 11 to remove personal text and include "Hello World" on 8/25/2022
+// 
 
 #include <iostream>
 using namespace std;
 
 int main() {
-	// is this note needed? :)
 	cout << "Hello, World!\n";
 	cout << "Welcome to C++ Programming Andrew Pettus! \n";
 	return 0;

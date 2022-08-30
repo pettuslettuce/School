e9a1d6d0e62527b9fba0a495089fbc78baa6f739
Andrew P <andrew.pettus@vubiqnetworks.com>
2022-08-30 11:17:31 -0700

    Chap2 Commit 1

---
+++ Chap2Lab1.cpp
@@ -0,0 +1,19 @@
+//  
+//  Chap2Lab1.cpp
+//  Written by Andrew Pettus on 8/30/2022
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // creating variables
+    string myName = "Andrew M. Pettus";
+    string myEmail = "apettus3@student.alamo.edu";
+   
+   // generating output text with variables
+   // whitespace in last position in quotes
+    cout << "My name is " << myName << ", and my email is " << myEmail << "\n";
+    return 0;
+}
---
+++ Chap2Lab2.cpp
@@ -0,0 +1,22 @@
+//  
+//  Chap2Lab2.cpp
+//  Written by Andrew Pettus on 8/30/2022
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //memory allocation
+    double GPA
+    double Age
+    
+    // creating variables
+    string myName = "Andrew M. Pettus";
+    float 
+
+
+    return 0;
+}

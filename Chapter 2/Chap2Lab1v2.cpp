a3e5ed26b7b996fbbbe169857f67bed03372d88a
Andrew P <andrew.pettus@vubiqnetworks.com>
2022-09-06 13:20:41 -0700

    Ch2Lab1v2
    Made alternative Ch2Lab1 assignment for endl function instead of "\n"

---
+++ Chapter 2/Chap2Lab1v2.cpp
@@ -0,0 +1,19 @@
+//  
+// Chap2Lab1.cpp
+// Written by Andrew Pettus on 8/30/2022
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // creating variables
+    string myName = "Andrew M. Pettus";
+    string myEmail = "apettus3@student.alamo.edu";
+   
+   // generating output text with variables
+   // whitespaces included at end of texts
+    cout << "My name is " << myName << ", and my email is " << myEmail << endl;
+    return 0;
+}

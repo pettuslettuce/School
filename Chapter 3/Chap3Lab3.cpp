c538b1eecc94c0d4345d2092b704a08dc424de9f
pettuslettuce <apettus3@student.alamo.edu>
2022-09-22 12:20:59 -0500

    Create Chap3Lab3.cpp

---
+++ Chapter 3/Chap3Lab3.cpp
@@ -0,0 +1,53 @@
+//  
+//  Chap3Lab1.cpp
+//  Written by Andrew Pettus on Sep/08/2022
+//
+//
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+int main() {
+    //variables
+    string userName;
+    string userStreet;
+    string userCity;
+    string userState;
+    string userZip;
+    //ask user for inputs
+    cout << "Please enter your full name: ";
+    getline(cin, userName);
+    cout << "Please enter your street address: ";
+    getline(cin, userStreet);
+    cout << "Please enter the city of the street address: ";
+    getline(cin, userCity);
+    cout << "Please enter the state of the street address: ";
+    getline(cin, userState);
+    cout << "Please enter the zip code of the street address: ";
+    getline(cin, userZip);
+    //envelope ready address output
+    //cout << "\n\nYour Mailing Label: \n\n";
+    // cout << userName << endl;
+    // cout << userStreet << endl;
+    // cout << userCity << ", " << userState << ", " << userZip << endl;
+
+    ofstream MyFile("TestFile.txt");
+
+    MyFile << "\n\nYour Mailing Label: \n\n"
+    << userName << endl
+    << userStreet << endl
+    << userCity << ", " << userState << ", " << userZip << endl;
+
+    MyFile.close();
+
+    string fileTextOne;
+
+    ifstream MyReadFile("TestFile.txt");
+
+    while (getline (MyReadFile, fileTextOne)) {
+        cout << fileTextOne << endl;
+    }
+    
+
+    return 0;
+}

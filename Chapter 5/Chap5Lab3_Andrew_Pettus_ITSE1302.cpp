8c0945092397f9c56e09b4e545b22b1d00f1d1dc
Andrew P <andrew.pettus@vubiqnetworks.com>
2022-10-27 11:50:34 -0700

    Labs

--- Chapter 5/Chap5Lab3_Andrew_Pettus_ITSE1302.cpp
+++ Chapter 5/Chap5Lab3_Andrew_Pettus_ITSE1302.cpp
@@ -1,13 +1,20 @@
-//  Chap5Lab1.cpp
+//  Chap5Lab3.cpp
 //  Written by Andrew Pettus on Oct/15/2022
 //
-/*Write a program that uses a while loops to perform the following steps:
-
-    Prompt the user to input two integers: firstNum and secondNum - (firstNum must be less than secondNum).
-    Output all odd numbers between firstNum and secondNum.
-    Output all even numbers between firstNum and secondNum.
-    Output the sum of all even numbers between firstNum and secondNum.
-    Output the sum of all odd numbers between firstNum and secondNum.
+/*
+Create a program "Box" that will ask the user for a character, as well as the length and Height of the box and output to the terminal the Box information and character based depiction of the box. 
+The program will tell the user the Length and Width of the box , indicate if it is a rectangle  or a square, and output the box using the character on the terminal.
+//
+Input:
+Length of Box
+Height of Box
+Character used for visual box display
+//
+Output:
+Box Length
+Box Height
+Message to the user that the box is either a rectangle or a square
+Visual Representation of the Box using the user identified character
 */
 //
 
@@ -18,8 +25,6 @@ using namespace std;
 int boxWidth = 0;
 int boxHeight = 0;
 char boxFill = '#';
-int widthCount = 0;
-int heightCount = 0;
 
 int main(){
 cout << "How wide would you like your box?" << endl;
@@ -31,19 +36,19 @@ cin >> boxFill;
 cout << "Your box's width is: " << boxWidth << endl;
 cout << "Your box's height is: " << boxHeight << endl;
 
-if (boxWidth == boxHeight){
+if (boxWidth == boxHeight){ // if box width is equal to its height, it's a square, duh
     cout << "This is a square" << endl;
 }
 else{
-    cout << "This is a rectangle" << endl;
+    cout << "This is a rectangle" << endl; // if width and height are not equal, it's nothing but a rectangle
 }
-for (int row = 0; row < boxHeight; row++)
+for (int row = 0; row < boxHeight; row++) // create "row" integer starting at 0, repeat while row number is less than box height, add 1 to "row" every iteration
 {
-    for (int column = 0; column < boxWidth; column++)
+    for (int column = 0; column < boxWidth; column++) // create "column" integer starting at 0, repeat while column number is less than box height, add 1 to "column" every iteration
     {
-        cout << boxFill << " ";
+        cout << boxFill << " "; // filling the box in the "columns" with the chose character in addition to a whitespace
     }
-    cout << endl;
+    cout << endl; // ends the line of the current "row", allowing "columns" to populate again in next row
 }
 return 0;
 }
---
+++ Chapter 6/Chap6Lab1_Andrew_Pettus_ITSE1302.cpp
@@ -0,0 +1,48 @@
+//  Chap6Lab1.cpp
+//  Written by Andrew Pettus on Oct/27/2022
+//
+// Input:
+// 1)Prompt the user for an initial savings account balance
+// 2)Prompt the user for a deposit amount
+// Process:
+// 1)Create a function that will take 2 arguments (current balance and deposit amount) and return the calculated balance of the account
+// 2)Create a void function as a program header:
+/*
+**********************************************
+                Alamo Bank                    
+**********************************************
+
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int accountBalance(int Balance, int Deposit);
+void printHeader();
+
+
+int main()
+{
+cout << printHeader << endl;
+cout << accountBalance << endl;
+}
+         
+int accountBalance(int Balance, int Deposit)
+{
+    int newBalance;
+    cout << "Please enter your initial balance: " << endl;
+    cin >> Balance;
+    cout << "Please enter your deposit amount: " << endl;
+    cin >> Deposit;
+    newBalance = Balance + Deposit;
+    cout << " Your new account balance is: " << newBalance << endl;
+    return 0;
+}
+
+void printHeader()
+{
+    cout << "**********************************************" << endl;
+    cout << "                Alamo Bank                    " << endl;
+    cout << "**********************************************" << endl;
+}

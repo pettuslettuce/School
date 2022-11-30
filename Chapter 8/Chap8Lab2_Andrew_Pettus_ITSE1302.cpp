b8a9f385f528fb56bbc2d6d3bcb5c12c0a0022ae
Andrew P <andrew.pettus@vubiqnetworks.com>
2022-11-30 09:45:25 -0800

    Labs

--- Chapter 8/Chap8Lab2_Andrew_Pettus_ITSE1302.cpp
+++ Chapter 8/Chap8Lab2_Andrew_Pettus_ITSE1302.cpp
@@ -1,6 +1,6 @@
 //  Chap8Lab2.cpp
 //  ITSE1302 NVC
-//  Written by Andrew Pettus on Nov/14/2022
+//  Written by Andrew Pettus on Nov/22/2022
 /*Create a program to use 2 arrays (string and double). Your String Array will hold the names of cars. 
 Your double array will hold the Miles Per Gallon (MPG) of the cars. Please note that the two arrays are parallel arrays 
     - in other words, the index 0 of the array to store the MPG will be associated to the index 0 of the car.
@@ -9,37 +9,20 @@ You will need to pre-populate 5 elements in each array. for this lab
     - I want you to create a function that will take two array arguments and output them as shown below. 
     The output will also indicate "Economical" or "Gas Guzzler"  depending on its fuel consumption. 
     If the car gets better than 30 MPG, indicate it is "Economical", otherwise indicate it is a "Gas Guzzler"
-
 Your output will use a function that takes the 2 Arrays as parameters
-
-Sample Output (2 of the 5 required elements):
-
-Car: Civic
-        MPG: 36.2
-        Economical
-Car: Tundra
-        MPGL 15.2
-        Gas Guzzler
 */
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
 const int ARRAY_SIZE = 5;
-
 string carName[ARRAY_SIZE] = {"Fork Fission", "Superoo Impressive", "Shevy Chase", "Hondo Acrid", "Owdi PP"};
 double carMPG[ARRAY_SIZE] = {25.4,34.1, 14.5, 31.1, 19.2};
-
 void carReport (string carName[], double carMPG[]);
-
 int main(){
-
 carReport(carName,carMPG);
-
 return 0;
 }
-
 void carReport (string carName[], double carMPG[]){
 
     for (int i = 0; i < ARRAY_SIZE; i++)
@@ -55,6 +38,5 @@ void carReport (string carName[], double carMPG[]){
             cout << "That's economical!" << endl;
         }       
     }
-
 }
 

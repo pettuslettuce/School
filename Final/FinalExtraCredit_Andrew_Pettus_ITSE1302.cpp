773526baa789717f1dcd67ceeccff0beaacb8f7d
pettuslettuce <apettus3@student.alamo.edu>
2022-12-02 17:24:41 -0600

    FinalEC

--- Final/FinalExtraCredit_Andrew_Pettus_ITSE1302.cpp
+++ Final/FinalExtraCredit_Andrew_Pettus_ITSE1302.cpp
@@ -6,9 +6,8 @@
 #include <iomanip>
 #include <string>
 using namespace std;
-
 const int ARRAY_SIZE = 5;
-
+int lotTotal;
 struct carDetails
 {
     string Make;
@@ -16,28 +15,23 @@ struct carDetails
     int Year;
     int price;
 };
-
 //Create a function that will display the entire inventory of the car lot
 void lotInventory(carDetails[], int);
 //Create a function that will show the total value of all of the car lots inventory (Total of all cars.)
-    //void lotValue(carDetails);
-
+void lotValuation(carDetails[], int);
 int main()
 {
     //Create an Array of 5 Car structures (you can pre-populate the data)
-    
     carDetails inventory[ARRAY_SIZE] = 
     {
         {"Fork", "Fission", 2013, 14999},
-        {"Superoo", "Impressive", 2018, 23000},
-        {"Owdi", "PP", 2014, 24500},
-        {"Shevy", "Chaser", 2020, 36975},
-        {"Edison", "Elephant", 1903, 34999}
+        {"Superoo", "Impressive", 2018, 23499},
+        {"Owdi", "PP", 2014, 34599},
+        {"Shevy", "Chaser", 2020, 32999},
+        {"Edison", "Elephant", 1903, 1000}
     };
-    
     lotInventory(inventory, ARRAY_SIZE);
-
-
+    lotValuation(inventory, ARRAY_SIZE);
 }
 
 void lotInventory(carDetails pCar[], int pSize)
@@ -45,10 +39,26 @@ void lotInventory(carDetails pCar[], int pSize)
     cout << "************************************ " << endl;
     cout << "Sleazy Sam's Used Car Lot Inventory: " << endl;
     cout << "************************************ " << endl;
-    cout << setw(12) << "Make: " << setw(18) << "Model: " << setw(8) << "Year: " << setw(10) << "Price: " << endl;
-    cout << pCar[0].Make << endl;
-
+    cout << setw(12) << left << "Make: " << setw(12) << left <<  "Model: " << setw(8) << left << "Year: " << setw(10) << left << "Price: " << endl;
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {   
+        cout << setw(12) << left << pCar[i].Make << setw(12) << left << pCar[i].Model << setw(8) << left << pCar[i].Year << setw(10) << left <<  pCar[i].price << endl;
+    }
 }
+
+void lotValuation(carDetails pCar[], int pSize)
+{
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {   
+        lotTotal = lotTotal + pCar[i].price;
+    }
+    cout << "************************************ " << endl;
+    cout << "Sleazy Sam's Used Car Lot Valuation: " << endl;
+    cout << "************************************ " << endl;
+    cout << "Total Value of Cars: $" << lotTotal << endl;
+}
+
+
 
 
 

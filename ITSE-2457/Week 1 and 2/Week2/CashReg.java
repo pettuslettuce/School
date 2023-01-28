208603835c3af403e1e19397b482a63d6e035a08
Andrew P <andrew.pettus@vubiqnetworks.com>
2023-01-27 22:39:24 -0800

    Module 2 Labs
    Module 2 Labs

---
+++ ITSE-2457/Week 1 and 2/Week2/CashReg.java
@@ -0,0 +1,15 @@
+/*
+ * ITSE-2457; OOP Java; M/W 2pm
+ * Written by Andrew Pettus
+ * Jan 27 2023
+ * 
+ * Cash Register Till Calculator
+ */
+
+
+ import java.util.Scanner;
+
+ public class CashReg {
+     System.out.println("Hey there Register Registrar! Let's count some cash! ");
+ }
+ 
---
+++ ITSE-2457/Week 1 and 2/Week2/CashReg/CashReg/src/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+rawerCounts[i]

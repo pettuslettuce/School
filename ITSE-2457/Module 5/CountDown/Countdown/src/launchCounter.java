ee91ffbd4e5540534d1094afe2f2f9fb41d15c88
pettuslettuce <apettus3@student.alamo.edu>
2023-03-05 18:33:14 -0600

    labs

--- ITSE-2457/Module 5/CountDown/Countdown/src/launchCounter.java
+++ ITSE-2457/Module 5/CountDown/Countdown/src/launchCounter.java
@@ -1,33 +1,55 @@
 public class launchCounter {
     public static void main(String[] args) throws Exception {
-
-    int counter = 10;
-
-    if (counter > 0){
-
-        do
-        {
-
-        for (int i = 0; i < 10; i++) {
-        
+        int doCounter = 10;
+        int whileCounter = 10;
+        // Do While
+        do {
+            System.out.printf("T- Minus: %d \n", doCounter);
+            doCounter--;
+            Thread.sleep(1000);
+            if (doCounter == 0) {
+                System.out.printf("T- Minus: %d \n", doCounter);
+                System.out.printf("WE HAVE IGNITION! \n");
+                Thread.sleep(1000);
+                System.out.printf("PAD LIFTOFF! \n");
+                Thread.sleep(1000);
+                System.out.printf("Vechile is nominal! \n");
+                Thread.sleep(1000);
+                System.out.printf("Do While Loop Example Complete! \n");
+                Thread.sleep(2000);
+            }
+        } while (doCounter > 0);
+        // For Loop
+        for (int i = 10; i > -1; i--) {
+            System.out.printf("T- Minus: %d \n", i);
+            Thread.sleep(1000);
+            if (i == 0) {
+                System.out.printf("WE HAVE IGNITION! \n");
+                Thread.sleep(1000);
+                System.out.printf("PAD LIFTOFF! \n");
+                Thread.sleep(1000);
+                System.out.printf("Vechile is nominal! \n");
+                Thread.sleep(1000);
+                System.out.printf("For Loop Example Complete! \n");
+                Thread.sleep(2000);
+            }
         }
-        System.out.printf("T- Minus: %d \n",counter);
-        counter--;
-        Thread.sleep(1000);
+        // While Loop
+        while (whileCounter > 0) {
+            System.out.printf("T- Minus: %d \n", whileCounter);
+            whileCounter--;
+            Thread.sleep(1000);
+            if (whileCounter == 0) {
+                System.out.printf("T- Minus: %d \n", whileCounter);
+                System.out.printf("WE HAVE IGNITION! \n");
+                Thread.sleep(1000);
+                System.out.printf("PAD LIFTOFF! \n");
+                Thread.sleep(1000);
+                System.out.printf("Vechile is nominal! \n");
+                Thread.sleep(1000);
+                System.out.printf("While Loop Example Complete! \n");
+                break;
+            }
         }
-        while (counter > -1);
-        
     }
-
-    else if (counter == 0){
-        System.out.printf("T- Minus: %d \n",counter);
-        System.out.printf("WE HAVE IGNITION!");
-        Thread.sleep(1000);
-        System.out.printf("LIFTOFF!");
-        Thread.sleep(1000);
-        System.out.printf("Vechile is nominal!");
-
-    }
-}
-
 }
